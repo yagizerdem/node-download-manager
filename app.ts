0b2173877e897efa6eb7bfaf01f0caac0b0b5227
yagiz erdem <2yagiz004@gmail.com>
2026-09-02 17:23:04 +0300

    fix: db.ts file deleation recovered

--- app.ts
+++ app.ts
@@ -1,4 +1,10 @@
+import { APP_DATA_DIR } from "./db";
+import fs from "fs";
 import { app, BrowserWindow } from "electron";
 import Main from "./main";
+
+if (!fs.existsSync(APP_DATA_DIR)) {
+  fs.mkdirSync(APP_DATA_DIR, { recursive: true });
+}
 
 Main.main(app, BrowserWindow);
