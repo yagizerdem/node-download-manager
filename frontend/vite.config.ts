2fd2c36feba8653d7140ee295f5be800c82980c5
yagiz erdem <2yagiz004@gmail.com>
2026-09-02 14:13:33 +0300

    init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: env.APP_PORT ? Number(env.APP_PORT) : 5173,
+    },
+  };
+});
